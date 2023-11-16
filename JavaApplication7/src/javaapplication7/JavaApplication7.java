baafeaa2798b4584d12c2e7a8502a2c79398fec3
khushi <sharmakhushi0418@gmai.com>
2023-11-16 18:04:02 +0530

    add new and delete un used files
    under 6 are working files (class13 to class17) but expect these files or classes they are not in use

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++
@@ -1,7 +0,0 @@
-package javaapplication7;
-public class JavaApplication7 {
-    public static void main(String[] args) {
-        
-    }
-    
-}
--- JavaApplication9fibonicciseries/src/javaapplication9fibonicciseries/JavaApplication9fibonicciseries.java
+++
@@ -1,9 +0,0 @@
-
-package javaapplication9fibonicciseries;
-public class JavaApplication9fibonicciseries{ 
-        
-    public static void main(String[] args) {
-        
-    }
-    
-}
