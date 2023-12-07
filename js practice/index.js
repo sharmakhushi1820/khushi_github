9e55d8daf69b928e275af87218fce3df5a57d992
khushi <sharmakhushi0418@gmai.com>
2023-12-07 18:51:22 +0530

    js practice topic

---
+++ js practice/index.js
@@ -0,0 +1,32 @@
+
+
+// window.alert("I REALLY LOVE MOMOS")
+
+
+// let firstName = "khushi";
+// let age = 14;
+// let student = true;
+
+// age = age + 1;
+
+// console.log("hello",firstName);
+// console.log( "you are " + age + " year old");
+// console.log( "there are 20 student in a class:",student);
+
+// document.getElementById("p1").innerHTML = "hello " + firstName;
+// document.getElementById("p2").innerHTML = "you are " + age + " year old";
+// document.getElementById("p3").innerHTML = "there is 20 student in a class: " + student;
+
+let students = 30;
+
+student = students + 1;
+student1 = students - 1;
+student2 = students * 1;
+student3 = students / 2;
+student4 = students % 2;
+
+document.write(student  +"<br>");
+document.write(student1 +"<br>");
+document.write(student2 +"<br>");
+document.write(student3 +"<br>");
+document.write(student4 +"<br>");
